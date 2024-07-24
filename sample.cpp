dc02d7474c460f1195973cb8d02f0a30467e248b
clg-sun-28 <sunpreet2809@gmail.com>
2024-07-24 15:02:38 +0530

    initial commit

---
+++ sample.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number -> ";
+    cin>>n;
+    cout<<"Count till number -> ";
+    for(int i=1;i<=n;i++){
+        cout<<i<<" ";
+    }
+}
